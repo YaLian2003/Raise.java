209129b61b9229ae65536b4a7b69318506faae59
YaLian2003 <54329751+YaLian2003@users.noreply.github.com>
2022-05-24 23:19:07 -0700

    Create StandingDesk.java

---
+++ StandingDesk.java
@@ -0,0 +1,11 @@
+// class StandingDesk
+public class StandingDesk{
+    // desk height
+    public int deskHeight;
+    
+    // constructor to initialize desk height
+    StandingDesk(int height){
+        this.deskHeight = height;
+    }
+    
+}
